77f622477d4e3ab435a4fb36e509b3bd8ab3f381
chenyuhan <yche0705@student.monash.edu>
2026-08-14 21:34:06 +0800

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/yuhan/fleetflow/FleetflowApplication.java
@@ -0,0 +1,13 @@
+package com.yuhan.fleetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetflowApplication.class, args);
+	}
+
+}
